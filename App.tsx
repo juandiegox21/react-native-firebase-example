d326dbf7d05755c9824f21198acac795dacd9bd5
Juan Atehortua <juandiegox17@gmail.com>
2021-07-25 23:24:40 -0400

    First Commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native'
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Tefst</Text>
+      <Text>Test</Text>
     </View>
   )
 }
